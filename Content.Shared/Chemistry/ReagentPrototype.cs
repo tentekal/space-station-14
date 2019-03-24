3fda5a6af6090b24dba3a791418160a3d54c7268
Acruid <shatter66@gmail.com>
2019-03-24 16:10:38 -0700

    Added the Solution class, as well as a ReagentPrototype class. Updated NuGet packages in Content.Tests.csproj

---
+++ Content.Shared/Chemistry/ReagentPrototype.cs
@@ -0,0 +1,25 @@
+﻿using SS14.Shared.Maths;
+using SS14.Shared.Prototypes;
+using SS14.Shared.Utility;
+using YamlDotNet.RepresentationModel;
+
+namespace Content.Shared.Chemistry
+{
+    [Prototype("reagent")]
+    public class ReagentPrototype : IPrototype, IIndexedPrototype
+    {
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public Color SubstanceColor { get; private set; }
+
+        public void LoadFrom(YamlMappingNode mapping)
+        {
+            ID = mapping.GetNode("id").AsString();
+            Name = mapping.GetNode("name").ToString();
+            Description = mapping.GetNode("desc").ToString();
+
+            SubstanceColor = mapping.TryGetNode("color", out var colorNode) ? colorNode.AsHexColor(Color.White) : Color.White;
+        }
+    }
+}
